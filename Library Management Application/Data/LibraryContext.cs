9768a009a02207c85c314c33e473d908cd24ec43
Nishadi Rathnayake <nishadirathnayake125@gmail.com>
2024-11-03 10:37:50 +0530

    Add files via upload

---
+++ Library Management Application/Data/LibraryContext.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryAPI.Data;
+using LibraryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class LibraryContext : DbContext
+{
+    public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) { }
+    public DbSet<Book> Books { get; set; }
+}
